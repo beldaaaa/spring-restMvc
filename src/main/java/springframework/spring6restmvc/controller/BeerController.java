3e886a8fa5c017350e13f7586496ca7dd45dc652
beldaaaa <oldbanana@email.cz>
2024-02-09 12:15:55 +0100

    MockMvc test for BeerController added

--- src/main/java/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/springframework/spring6restmvc/controller/BeerController.java
@@ -1,6 +1,7 @@
 package springframework.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController//Rest => Spring now knows it has to return back proper response body (JSON and not HTML)
 @RequestMapping("/api/v1/beer")
 public class BeerController {
--- src/test/java/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -2,17 +2,32 @@ package springframework.spring6restmvc.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import springframework.spring6restmvc.services.BeerService;
 
 import java.util.UUID;
 
-@SpringBootTest
-class BeerControllerTest {
-    @Autowired
-    BeerController beerController;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;//for some reason this static import has to be done manually
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;//same here
 
+
+@WebMvcTest(BeerController.class)//I want to limit this to BeerController class
+class BeerControllerTest {
+
+@Autowired
+MockMvc mockMvc;//Now I want to bring in a dependency of autowired on MockMvc
+@MockBean//tells Mockito to provide a mock of this into the Spring context
+        //without this mock bean I would get an exception saying that I dont have that dependency there
+        //and I woud have to provide it manually
+BeerService beerService;
     @Test
-    void getBearById() {
-        System.out.println(beerController.getBearById(UUID.randomUUID()));
+    void getBearById() throws Exception {
+
+        mockMvc.perform(get("/api/v1/beer/"+ UUID.randomUUID())//I want ot perform get against URL
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());//and I should get back an OK status
     }
 }
