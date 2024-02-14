a41b899bba40a729ae272b85355b4142459b49f1
beldaaaa <oldbanana@email.cz>
2024-02-14 12:00:44 +0100

    PATCH test added

--- src/test/java/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,6 +14,8 @@ import springframework.spring6restmvc.model.Beer;
 import springframework.spring6restmvc.services.BeerService;
 import springframework.spring6restmvc.services.BeerServiceImpl;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,31 +34,48 @@ class BeerControllerTest {
 
     @Autowired
     MockMvc mockMvc;//Now I want to bring in a dependency of autowired on MockMvc
-    @MockBean//tells Mockito to provide a mock of this into the Spring context
-    //without this mock bean I would get an exception saying that I dont have that dependency there
+    @MockBean//tells Mockito to provide mock of this into the Spring context
+    //without this mock bean I would get an exception saying that I don't have that dependency there,
     //and I would have to provide it manually
     BeerService beerService;
     @Autowired
     ObjectMapper objectMapper;
     BeerServiceImpl beerServiceImpl;
 
+    @Captor
+    ArgumentCaptor<UUID> uuidArgumentCaptor;// to have a reusable component, which means I no longer need
+    //declaration in delete test method
+
+    @Captor
+    ArgumentCaptor<Beer> beerArgumentCaptor;
+
     @BeforeEach
     void setUp() {
         beerServiceImpl = new BeerServiceImpl();
     }
 
-    //    @Test
-//    void testDeleteBeer() throws Exception {
-//        Beer beer = beerServiceImpl.beerList().getFirst();
-//
-//        mockMvc.perform(delete("/api/v1/beer/" + beer.getId())
-//                        .accept(MediaType.APPLICATION_JSON)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(beer)))
-//                .andExpect(status().isNoContent());
-//
-//        verify(beerService).deleteById(any(UUID.class));
-//    }
+    @Test
+    void patchBeer() throws Exception {
+        Beer beer = beerServiceImpl.beerList().getFirst();//to give my request a body (JSON of the beer map object)
+        //a little change: I don't need to give it a fully qualified object,
+        //so I can just create a map for Jackson (put key becomes the JSON property)
+        Map<String, Object> beerMap = new HashMap<>();
+        //now I'll get a simple JSON object being passed in with just beerName(which is mimicking what clients would do)
+        //they are not going to send out a fully qualified object (only properties they want to change)
+        beerMap.put("beerName", "New Name");
+
+        mockMvc.perform(patch("/api/v1/beer/" + beer.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerMap)))
+                .andExpect(status().isNoContent());
+
+        verify(beerService).patchById(uuidArgumentCaptor.capture(), beerArgumentCaptor.capture());
+
+        assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+        assertThat(beerMap.get("beerName")).isEqualTo(beerArgumentCaptor.getValue().getBeerName());
+    }
+
     @Test
     void deleteBeer() throws Exception {//delete operation is probably the simplest REST operation
         Beer beer = beerServiceImpl.beerList().getFirst();
@@ -65,8 +85,9 @@ class BeerControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)//doesn't work without
                         .content(objectMapper.writeValueAsString(beer)))//those 2 lines (but it should?)
                 .andExpect(status().isNoContent());
-
-        ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);//argument captor created as Mockito class
+//commented after ArgumentCaptor was defined above;
+        //ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);//argument captor
+        //created as Mockito class
         //Using argument captor can compare the beer's id is equal to the one that is passed as a parameter to the URL.
         verify(beerService).deleteById(uuidArgumentCaptor.capture());//sit on that mock and listen for anything what is passed in
         //and ensure to pass proper UUID value
@@ -109,15 +130,15 @@ class BeerControllerTest {
         Beer testBeer = beerServiceImpl.beerList().getFirst();
 
         given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);//configuring Mockito to go ahead
-        // and return back that testBeer object. So I get the testBeer object from the service implementation and then
-        //tell Mockito to return it
+        // and return that testBeer object.
+        // So I get the testBeer object from the service implementation and then tell Mockito to return it
         mockMvc.perform(get("/api/v1/beer/" + testBeer.getId())//I want ot perform get against URL
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())//and I should get back an OK status
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
                 .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
-        //this gets a beer object to return back from my Mock MVC controller
+        //this gets a beer object to return from my Mock MVC controller
     }
 
     @Test
@@ -133,7 +154,7 @@ class BeerControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(beer)))
                 .andExpect(status().isCreated());
-//                .andExpect(header().exists("Location"));//doesnt work
+//                .andExpect(header().exists("Location"));//doesn't work
     }
 
 }
--- src/test/java/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -3,6 +3,7 @@ package springframework.spring6restmvc.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,6 +13,8 @@ import springframework.spring6restmvc.model.Customer;
 import springframework.spring6restmvc.services.CustomerService;
 import springframework.spring6restmvc.services.CustomerServiceImpl;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,6 +37,28 @@ public class CustomerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Captor
+    ArgumentCaptor<UUID> uuidArgumentCaptor;
+    @Captor
+    ArgumentCaptor<Customer> customerArgumentCaptor;
+
+    @Test
+    void patchCustomer() throws Exception {
+        Customer customer = customerServiceImpl.customerList().getFirst();
+        Map<String, Object> customerMap = new HashMap<>();
+        customerMap.put("customerName", "Fakt se tak jmenuje");
+
+        mockMvc.perform(patch("/api/v1/customer/" + customer.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customerMap)))
+                .andExpect(status().isNoContent());
+
+        verify(customerService).patchById(uuidArgumentCaptor.capture(), customerArgumentCaptor.capture());
+
+        assertThat(customer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+        assertThat(customerMap.get("customerName")).isEqualTo(customerArgumentCaptor.getValue().getCustomerName());
+    }
 
     @Test
     void deleteCustomer() throws Exception {
@@ -45,7 +70,6 @@ public class CustomerControllerTest {
                         .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isNoContent());
 
-        ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);
         verify(customerService).deleteById(uuidArgumentCaptor.capture());
         assertThat(customer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
     }
@@ -75,7 +99,7 @@ public class CustomerControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isCreated());
-//                .andExpect(header().exists("Location"));//doesnt work
+//                .andExpect(header().exists("Location"));//doesn't work
     }
 
     @Test
